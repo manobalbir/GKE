52a976f5dbed947ef0f846612a88bffabe9111c0
Balbir singh <bsingharora@gmail.com>
2021-03-18 13:47:37 +1100

    Add identity service
    two steps still fail
    
    1. workload identitiy mapping
    2. gcloud builds submit with --tag
    
    Signed-off-by: Balbir singh <bsingharora@gmail.com>

--- trigger-func/main.sh
+++ trigger-func/main.sh
@@ -1,2 +1,29 @@
 # from https://cloud.google.com/kubernetes-engine/docs/how-to/workload-identity
+APP="trigger-func"
+TOPIC="feedback-created"
+PROJECT="root-melody-306922"
+
+gcloud config set project $PROJECT
+kubectl create serviceaccount $APP
+gcloud iam service-accounts create $APP
+
+GSA="$APP@$PROJECT.iam.gserviceaccount.com"
+
+gcloud projects add-iam-policy-binding $PROJECT\
+  --member "serviceAccount:$GSA" --role roles/datastore.user
+
+gcloud pubsub topics add-iam-policy-binding $TOPIC \
+  --member "serviceAccount:$GSA" --role roles/pubsub.publisher
+
+gcloud iam service-accounts add-iam-policy-binding \
+  --role roles/iam.workloadIdentityUser \
+  --member "serviceAccount:$PROJECT.svc.id.goog[default/$APP]" \
+  $GSA
+
+kubectl annotate serviceaccount \
+  $APP iam.gke.io/gcp-service-account=$GSA --overwrite
+
+TAG="gcr.io/$PROJECT/$APP"
+gcloud builds submit --tag $TAG
+kubectl apply --filename service.yaml
 
